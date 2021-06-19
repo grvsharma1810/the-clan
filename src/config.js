a1b3db7a7b00af55e09fa8ed31dcf38f69779aa2
grvsharma1810 <gs8280601@gmail.com>
2021-06-19 11:35:09 +0530

    fix: API URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://the-clan-backend.herokuapp.com"
-export const API_URL = "http://localhost:3001"
+export const API_URL = "https://the-clan-backend.herokuapp.com"
+// export const API_URL = "http://localhost:3001"
