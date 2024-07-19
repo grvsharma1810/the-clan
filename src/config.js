aed0ae8d8253b8086bddd8ef71cd15e6ea57efd1
Gaurav <gs8280601@gmail.com>
2024-07-19 17:48:39 +0530

    Rebirth

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://the-clan-backend.herokuapp.com"
+export const API_URL = "https://the-clan-backend-production.up.railway.app"
 // export const API_URL = "http://localhost:3001"
